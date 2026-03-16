2c60f7a886fec83805f71f3f6960be36461daa50
monkeyjack123 <jack.pu@kellogg.northwestern.edu>
2026-03-16 22:48:25 +0000

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,21 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all
   end
 
   def new
+    if @current_user == nil
+      flash["notice"] = "You must login first."
+      redirect_to "/login"
+    end
   end
-  
+
   def create
     @post = Post.new
     @post["body"] = params["body"]
     @post["image"] = params["image"]
-    # TODO: assign logged-in user as user that created the post
+    @post["user_id"] = @current_user["id"]
+
     @post.save
     redirect_to "/posts"
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,26 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # TODO: authenticate user
-    flash["notice"] = "Nope."
+    user = User.find_by({ "email" => params["email"] })
+
+    if user == nil
+      flash["notice"] = "No account found with that email."
+      redirect_to "/login"
+    else
+      if user.authenticate(params["password"])
+        session["user_id"] = user["id"]
+        flash["notice"] = "Logged in."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Wrong password."
+        redirect_to "/login"
+      end
+    end
+  end
+
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Logged out."
     redirect_to "/login"
   end
 end
